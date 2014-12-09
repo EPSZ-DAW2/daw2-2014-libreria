225236832e83364c2d1a0cd5f538c95962f45392
alessrv188@gmail.com <alessrv188@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2014-12-09 19:06:45 +0000

    modificaciones view libros

--- trunk/protected/views/libro/view.php
+++ trunk/protected/views/libro/view.php
@@ -16,14 +16,14 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Libro #<?php echo $model->IdLibro; ?></h1>
+<h1>View Libro <?php echo $model->Titulo; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
 		'IdLibro',
 		'ISBN',
-		'IdEditorial',
+		array( 'name'=>'IdEditorial', 'value'=>(($model->editorial !== null) ? $model->editorial->Nombre : '')),
 		'Titulo',
 		'Edicion',
 		'Paginas',

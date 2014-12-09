65b2dabcfb984219117bd7ed126f4c3f96b15184
verubuntu@gmail.com <verubuntu@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2014-12-09 18:57:27 +0000

    Cambio en la visualización y menús

---
+++ trunk/protected/views/site/registro.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+
+$this->pageTitle=Yii::app()->name . ' - Registro';
+$this->breadcrumbs=array(
+	'Registro',
+);
+?>
+
+<h2>Registro <?php echo $code; ?></h2>

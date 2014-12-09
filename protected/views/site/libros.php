1e6f70a9ed11a91aeac13f9c80aaecf7cdee6dc1
verubuntu <verubuntu@gmail.com>
2014-12-09 18:57:27 +0000

    Cambio en la visualización y menús

---
+++ protected/views/site/libros.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this SiteController */
+/* @var $model Libros */
+/* @var $form CActiveForm */
+
+$this->pageTitle=Yii::app()->name . ' - Libros';
+$this->breadcrumbs=array(
+	'Libros',
+);
+?>
+
+<h2>Libros <?php echo $code; ?></h2>

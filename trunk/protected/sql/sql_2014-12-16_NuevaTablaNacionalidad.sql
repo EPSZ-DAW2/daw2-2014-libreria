703da31c1d6efe1d5b141e64b78df0dbc188936a
dgarciasarai@gmail.com <dgarciasarai@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2014-12-16 17:44:38 +0000

    Cambios en BBDD, y nuevos datos para las tablas: Autor y Nacionalidad

---
+++ trunk/protected/sql/sql_2014-12-16_NuevaTablaNacionalidad.sql
@@ -0,0 +1,24 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+ALTER TABLE `libreria`.`autor` CHANGE COLUMN `Nacionalidad` `IdNacionalidad` INT(11) NOT NULL  , 
+  ADD CONSTRAINT `fk_Autor_Nacionalidad`
+  FOREIGN KEY (`IdNacionalidad` )
+  REFERENCES `libreria`.`nacionalidad` (`IdNacionalidad` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `fk_Autor_Nacionalidad_idx` (`IdNacionalidad` ASC) ;
+
+CREATE  TABLE IF NOT EXISTS `libreria`.`nacionalidad` (
+  `IdNacionalidad` INT(11) NOT NULL ,
+  `NombreNacionalidad` VARCHAR(45) NOT NULL ,
+  PRIMARY KEY (`IdNacionalidad`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_general_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

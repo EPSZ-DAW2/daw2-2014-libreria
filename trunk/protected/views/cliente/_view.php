bfbcee3ae784344913dee0d08d0315df737888e9
dqmzamora@gmail.com <dqmzamora@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2014-12-23 18:16:33 +0000

    Actualización de mantenimiento de Clientes

--- trunk/protected/views/cliente/_view.php
+++ trunk/protected/views/cliente/_view.php
@@ -5,8 +5,8 @@
 
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('IdCliente')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->IdCliente), array('view', 'id'=>$data->IdCliente)); ?>
+	<b><?php echo CHtml::encode($data->cliente->getAttributeLabel('Nombre')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->cliente->Nombre.' '.$data->cliente->Apellidos), array('view', 'id'=>$data->IdCliente)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('DomicilioFacturacion')); ?>:</b>

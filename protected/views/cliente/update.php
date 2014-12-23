68ade979e7605169817012fae74365fc14bd72aa
dqmzamora <dqmzamora@gmail.com>
2014-12-23 18:16:33 +0000

    Actualización de mantenimiento de Clientes

--- protected/views/cliente/update.php
+++ protected/views/cliente/update.php
@@ -4,18 +4,18 @@
 
 $this->breadcrumbs=array(
 	'Clientes'=>array('index'),
-	$model->IdCliente=>array('view','id'=>$model->IdCliente),
-	'Update',
+	$model->cliente->Nombre=>array('view','id'=>$model->IdCliente),
+	'Actualizar',
 );
 
 $this->menu=array(
-	array('label'=>'List Cliente', 'url'=>array('index')),
-	array('label'=>'Create Cliente', 'url'=>array('create')),
-	array('label'=>'View Cliente', 'url'=>array('view', 'id'=>$model->IdCliente)),
-	array('label'=>'Manage Cliente', 'url'=>array('admin')),
+	array('label'=>'Listar Clientes', 'url'=>array('index')),
+	array('label'=>'Crear Cliente', 'url'=>array('create')),
+	array('label'=>'Ver Cliente', 'url'=>array('view', 'id'=>$model->IdCliente)),
+	array('label'=>'Gestionar Clientes', 'url'=>array('admin')),
 );
 ?>
 
-<h1>Update Cliente <?php echo $model->IdCliente; ?></h1>
+<h1>Actualizar Cliente: <?php echo $model->cliente->Nombre; ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>

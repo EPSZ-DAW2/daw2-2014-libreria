da1376886d18feb3c4b16d54d4d1e7619a681c86
jandrolc91 <jandrolc91@gmail.com>
2014-12-27 17:00:12 +0000

    Añadido formulario de registro

--- protected/views/site/registro.php
+++ protected/views/site/registro.php
@@ -1,12 +1,69 @@
 <?php
 /* @var $this SiteController */
-/* @var $model LoginForm */
+/* @var $model RegistroForm */
 /* @var $form CActiveForm  */
 
 $this->pageTitle=Yii::app()->name . ' - Registro';
-$this->breadcrumbs=array(
-	'Registro',
-);
+$this->breadcrumbs=array('registro');
 ?>
 
-<h2>Registro <?php echo $code; ?></h2>
+<h1>Registro</h1>
+
+<div class="form">
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'registro-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<p class="note">Los campos marcados con <span class="required">*</span> son obligatorios.</p>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email'); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+	<div class="row">
+		<?php echo $form->labelEx($model,'nombre'); ?>
+		<?php echo $form->textField($model,'nombre'); ?>
+		<?php echo $form->error($model,'nombre'); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->labelEx($model,'apellidos'); ?>
+		<?php echo $form->textField($model,'apellidos'); ?>
+		<?php echo $form->error($model,'apellidos'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password'); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->labelEx($model,'repetir_password'); ?>
+		<?php echo $form->passwordField($model,'repetir_password'); ?>
+		<?php echo $form->error($model,'repetir_password'); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->labelEx($model,'nif'); ?>
+		<?php echo $form->textField($model,'nif'); ?>
+		<?php echo $form->error($model,'nif'); ?>
+	</div>
+	
+	<div class="row">
+		<?php echo $form->labelEx($model,'telefono'); ?>
+		<?php echo $form->textField($model,'telefono'); ?>
+		<?php echo $form->error($model,'telefono'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Enviar'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+</div><

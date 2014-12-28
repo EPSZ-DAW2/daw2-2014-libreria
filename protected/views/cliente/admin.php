839730c347989a24892055d1b9a0f06f62354e35
dgarciasarai <dgarciasarai@gmail.com>
2014-12-28 18:45:07 +0000

    Restauarción de elementos de la vista de "Clientes". Tened cuidado con las confirmaciones...Primero actualizar

--- protected/views/cliente/admin.php
+++ protected/views/cliente/admin.php
@@ -4,7 +4,7 @@
 
 $this->breadcrumbs=array(
 	'Clientes'=>array('index'),
-	'Manage',
+	'Gestionar Clientes',
 );
 
 $this->menu=array(
@@ -12,35 +12,19 @@ $this->menu=array(
 	array('label'=>'Crear Cliente', 'url'=>array('create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#cliente-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
 ?>
 
-<h1>Gestionar Clientes</h1>
 
-<?php echo CHtml::link('Búsqueda Avanzada','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+<h1>Gestionar Clientes</h1>
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'cliente-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'IdCliente',
+		'cliente.Nombre',
+		'cliente.Apellidos',
+		'cliente.NIF',
 		'DomicilioFacturacion',
 		'CPFacturacion',
 		'PoblacionFacturacion',

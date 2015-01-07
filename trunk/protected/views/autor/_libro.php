fd24310b26ad484357091e6ba3040bc4edfe941d
dgarciasarai@gmail.com <dgarciasarai@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2015-01-07 14:57:07 +0000

    Arreglado link para ver un libro desde un autor

--- trunk/protected/views/autor/_libro.php
+++ trunk/protected/views/autor/_libro.php
@@ -6,13 +6,13 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Titulo')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->Titulo), array('view', 'id'=>$data->IdLibro)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->Titulo), array('libro/view', 'id'=>$data->IdLibro)); ?>
 	<br />
-	
+
 	<b><?php echo CHtml::encode($data->getAttributeLabel('ISBN')); ?>:</b>
 	<?php echo CHtml::encode($data->ISBN); ?>
 	<br />
-	
+
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Formato')); ?>:</b>
 	<?php echo CHtml::encode($data->Formato); ?>
 	<br />
@@ -24,7 +24,7 @@
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Paginas')); ?>:</b>
 	<?php echo CHtml::encode($data->Paginas); ?>
 	<br />
-	
+
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Precio')); ?>:</b>
 	<?php echo CHtml::encode(number_format($data->Precio, 2, ',', ' ').'€'); ?>
 	<br />

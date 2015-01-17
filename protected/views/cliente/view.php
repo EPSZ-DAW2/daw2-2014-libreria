b5a78bdd06d59b6b42f941c2a86fb960ba14860f
verubuntu <verubuntu@gmail.com>
2015-01-17 17:44:48 +0000

    Añadido formulario inferior para que el cliente pueda cambiar sus datos (falta hacer update en BBDD)

--- protected/views/cliente/view.php
+++ protected/views/cliente/view.php
@@ -16,6 +16,7 @@ $this->menu=array(
 );
 ?>
 
+
 <h1>Cliente: <?php echo $model->usuario->Nombre.' '.$model->usuario->Apellidos; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
@@ -39,3 +40,48 @@ $this->menu=array(
 			),
 	),
 )); ?>
+
+<br/>
+<h1>Editar datos:</h1>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'cliente-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Los campos con <span class="required">*</span> son obligatorios.</p>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'DomicilioFacturacion'); ?>
+		<?php echo $form->textField($model,'DomicilioFacturacion',array('size'=>60,'maxlength'=>100,'placeholder'=>'C/Roble, 5 1D')); ?>
+		<?php echo $form->error($model,'DomicilioFacturacion'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'CPFacturacion'); ?>
+		<?php echo $form->textField($model,'CPFacturacion',array('maxlength'=>5,'placeholder'=>'49003')); ?>
+		<?php echo $form->error($model,'CPFacturacion'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'PoblacionFacturacion'); ?>
+		<?php echo $form->textField($model,'PoblacionFacturacion',array('size'=>60,'maxlength'=>60,'placeholder'=>'Zamora')); ?>
+		<?php echo $form->error($model,'PoblacionFacturacion'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'ProvinciaFacturacion'); ?>
+		<?php echo $form->textField($model,'ProvinciaFacturacion',array('size'=>40,'maxlength'=>40,'placeholder'=>'Zamora')); ?>
+		<?php echo $form->error($model,'ProvinciaFacturacion'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Crear' : 'Guardar'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div>
+

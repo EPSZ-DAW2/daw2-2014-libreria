ad8e0dfa419745b67ad3bb8fc8df75fe64d8d891
verubuntu@gmail.com <verubuntu@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2015-01-17 19:48:20 +0000

    Añadidas 2 portadas de libros al portal principal para mejorar un poco el aspecto.

--- trunk/protected/views/site/index.php
+++ trunk/protected/views/site/index.php
@@ -5,3 +5,13 @@ $this->pageTitle=Yii::app()->name;
 ?>
 
 <h1>Bienvenido a la <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<div id="portada">
+	<div id="principal">
+		<img src="images/portada.png">
+	</div>
+	<div id="menu-left">
+		<a href="index.php?r=libro/view&id=4"><img src="images/portadas/4.png" width="140px"></a><br/><br/>
+		<a href="index.php?r=libro/view&id=9"><img src="images/portadas/9.png" width="140px"></a>
+	</div>
+</div>

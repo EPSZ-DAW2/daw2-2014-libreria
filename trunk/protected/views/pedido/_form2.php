e01c05a0bad21bb2dd728addb469026fc25f6057
l.giglio.a@gmail.com <l.giglio.a@gmail.com@42214c44-5dce-0d73-d85b-588ff84e726b>
2015-01-18 15:06:40 +0000

    Tabla linea modificada y cambios en la gestión de pedidos

--- trunk/protected/views/pedido/_form2.php
+++ trunk/protected/views/pedido/_form2.php
@@ -9,18 +9,21 @@
 
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'id'=>'pedido-form',
+	'focus'=> array($model,'IdPedido'),
 	// Please note: When you enable ajax validation, make sure the corresponding
 	// controller action is handling ajax validation correctly.
 	// There is a call to performAjaxValidation() commented in generated controller code.
 	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
+	'enableClientValidation'=>true,
+	'enableAjaxValidation'=>true,
 )); ?>
 
 	<p class="note"><span class="required">*</span>Campos obligatorios.</p>
 
 	<?php echo $form->errorSummary($model); ?>
 
-	<h2>Cliente: <?php echo $model->usuario->Nombre.' '.$model->usuario->Apellidos;?></h2>
+	<h2>Cliente: <?php echo $model->usuario->Nombre.' '.$model->usuario->Apellidos;?></h2></br>
+	<h3>Fecha del pedido: <?php echo $model->Fecha;?></h3>
 	<hr>
 	<div class="row">
 		<?php echo $form->labelEx($model,'Forma Pago'); ?>
@@ -61,7 +64,7 @@
 		<?php echo $form->error($model,'TelefonoEnvio'); ?>
 	</div>
 	<hr><h2>Libros del Pedido</h2><hr>
-	<?php $lineas = $model->lineas;  
+	<?php /*$lineas = $model->lineas;  
 	foreach($lineas as $pedidoL) {?>
 	<div class="row">
 		<?php echo $form->labelEx($pedidoL,'Titulo'); ?>
@@ -106,6 +109,20 @@
                         $pedidoLinea->save();
                     }*/
 	?>
+	
+	    <div id="linea">
+        <?php
+        $index = 0;
+        foreach ($model->lineas as $id => $linea):
+            $this->renderPartial('linea/_form', array(
+                'model' => $linea,
+                'index' => $id,
+                'display' => 'block'
+            ));
+            $index++;
+        endforeach;
+		?>
+    </div>
 	
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Crear' : 'Guardar'); ?>
--- trunk/protected/views/pedido/_view.php
+++ trunk/protected/views/pedido/_view.php
@@ -4,15 +4,13 @@
 ?>
 
 <div class="view">
-<!--
-		Cliente y gestión
-		if (Yii::app()->user->checkAccess( 'sysadmin') || Yii::app()->user->checkAccess( 'admin') || Yii::app()->user->checkAccess( 'vendedor') || Yii::app()->user->checkAccess( 'gerente')){
--->
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('IdPedido')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->IdPedido), array('view', 'id'=>$data->IdPedido)); ?>
-	<br />
+	<b><?php echo CHtml::encode('Ver Pedido'); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->IdPedido.'-'.$data->Serie.'-'.$data->Numero), 
+	array('view', 'id'=>$data->IdPedido)); ?>
+	<br /><br />
 
+	<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Serie')); ?>:</b>
 	<?php echo CHtml::encode($data->Serie); ?>
 	<br />
@@ -20,39 +18,40 @@
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Numero')); ?>:</b>
 	<?php echo CHtml::encode($data->Numero); ?>
 	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('Fecha')); ?>:</b>
-	<?php echo CHtml::encode($data->Fecha); ?>
+	
+	<b><?php echo CHtml::encode($data->getAttributeLabel('IdCliente')); ?>:</b>
+	<?php echo CHtml::encode($data->IdCliente); ?>
 	<br />
-<!--
-	<b><?php //echo CHtml::encode($data->getAttributeLabel('IdCliente')); ?>:</b>
-	<?php //echo CHtml::encode($data->IdCliente); ?>
-	<br />
--->	
+	*/?>
+	
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Cliente')); ?>:</b>
 	<?php echo CHtml::encode($data->usuario->Nombre).'&nbsp';
 	      echo CHtml::encode($data->usuario->Apellidos);?>
 	<br />
 	
-	<b><?php echo CHtml::encode($data->getAttributeLabel('Forma pago')); ?>:</b>
+	<b><?php echo CHtml::encode($data->getAttributeLabel('Fecha')); ?>:</b>
+	<?php echo CHtml::encode($data->Fecha); ?>
+	<br />
+	
+	<b><?php echo CHtml::encode($data->getAttributeLabel('Forma de pago')); ?>:</b>
 	<?php echo CHtml::encode($data->pago->Nombre); ?>
 	<br />
 
+	<b><?php echo CHtml::encode($data->getAttributeLabel('Estado del Pedido')); ?>:</b>
+	<?php echo CHtml::encode($data->estado->Nombre); ?>
+	<br />
+	
+	<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('IVA')); ?>:</b>
 	<?php echo CHtml::encode($data->IVA); ?>
 	<br />
 
-	<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('GastosEnvio')); ?>:</b>
 	<?php echo CHtml::encode($data->GastosEnvio); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('Pagado')); ?>:</b>
 	<?php echo CHtml::encode($data->Pagado); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('IdEstado')); ?>:</b>
-	<?php echo CHtml::encode($data->IdEstado); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('DomicilioEnvio')); ?>:</b>
